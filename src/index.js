35ba42b2bcc82a85a5a05a7ef2e95636ffbe7603
BudwinDeSaram <dinura.20210387@iit.ac.lk>
2025-03-16 23:50:38 +0530

    Added asgardeo configuration

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css"; // TailwindCSS or other styles
+import { AuthProvider } from "@asgardeo/auth-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AuthProvider
+        config={ {
+            signInRedirectURL: "https://468223a9-7ed7-4f9e-bb6d-aa85cb7415a6.e1-us-east-azure.choreoapps.dev/",
+            signOutRedirectURL: "https://468223a9-7ed7-4f9e-bb6d-aa85cb7415a6.e1-us-east-azure.choreoapps.dev/",
+            clientID: "l1au16BXengfS533ifKcXtT27Yka",
+            baseUrl: "https://api.asgardeo.io/t/budwin",
+            scope: [ "openid","profile" ]
+        } }
+    >
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </AuthProvider>
 );
