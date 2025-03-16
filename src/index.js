9a24e43f3876ea35ada15fee53ef6d033e496b18
BudwinDeSaram <dinura.20210387@iit.ac.lk>
2025-03-16 23:58:44 +0530

    Modified index

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import "./index.css"; // TailwindCSS or other styles
 import { AuthProvider } from "@asgardeo/auth-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <AuthProvider
+root.render(  
+    <React.StrictMode>
+      <AuthProvider
         config={ {
             signInRedirectURL: "https://468223a9-7ed7-4f9e-bb6d-aa85cb7415a6.e1-us-east-azure.choreoapps.dev/",
             signOutRedirectURL: "https://468223a9-7ed7-4f9e-bb6d-aa85cb7415a6.e1-us-east-azure.choreoapps.dev/",
@@ -14,9 +15,8 @@ root.render(
             baseUrl: "https://api.asgardeo.io/t/budwin",
             scope: [ "openid","profile" ]
         } }
-    >
-      <React.StrictMode>
+      >
         <App />
-      </React.StrictMode>
-    </AuthProvider>
+      </AuthProvider>
+    </React.StrictMode>    
 );
